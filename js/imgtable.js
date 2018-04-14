63bf050084b0540284148fff85f5cd86ba451a31
admin <1005939167@qq.com>
2018-04-14 10:35:54 +0800

    2018年4月4日

--- js/imgtable.js
+++ js/imgtable.js
@@ -1,8 +1,8 @@
-(function(){
-    new Vue({
-        el:"table",
-        data:{
-            msg:[]
-        }
+$(function(){
+    // 删除节点
+    $('table tr td:last-child img').each(function(k){
+        $(this).click(function(){
+            $('table tr td:last-child img').eq(k).parents('tr').empty();
+        })
     })
-})()
+})
